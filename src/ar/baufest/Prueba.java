bf8ce44aff31b66efe88b5b6795d69f7a33a3b6d
rnfanego <rnfanego@gmail.com>
2014-01-13 23:36:32 -0300

    aasdfwqe

--- src/ar/baufest/Prueba.java
+++ src/ar/baufest/Prueba.java
@@ -4,6 +4,14 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Curso git");
+		sdafasdfasdfasdf
+sadfasdfasdfasdf
+sadf
+asdf
+asd
+fasd
+f
+sadfasdfasdfasdfasdfsadfasdfsa
 	}
 
 }
