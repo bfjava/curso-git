f7bd8d83eb2fe0cb2ee649a5e11f313b0f6e5033
mgomez@baufest.com <mgomez@BFNB11893.baunet.local>
2014-01-14 13:27:30 -0300

    llegue?

--- src/ar/baufest/Prueba.java
+++ src/ar/baufest/Prueba.java
@@ -4,6 +4,11 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Curso Git");
+System.out.println("Curso Git6");
+System.out.println("Curso Git5");
+System.out.println("Curso Git4");
+System.out.println("Curso Git3");
+System.out.println("Curso Git2");
 	}
 
 }
