8409bb211a232bcb5b65a3dddb011716d934e9fe
ruben.fanego <ruben.fanego@mercadolibre.com>
2014-01-14 13:28:02 -0300

    commit conflict

--- src/ar/baufest/Prueba.java
+++ src/ar/baufest/Prueba.java
@@ -4,6 +4,14 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Curso Git");
+		askljñdfñlkadsjflñsadkjfasd
+		f
+		sadf
+		sad
+		f
+		asd
+		f
+		asdfasdfasdfasdfasdfsadfadsfsadf
 	}
 
 }
